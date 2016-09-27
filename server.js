a45279f9cdf20cfe1d466ce9b9e34ecb9e451c77
arunsamk <arunsamk@gmail.com>
2016-09-27 23:08:50 +0530

    27-09-2016-home

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var application = express();
+var path = require('path');
+
+//Making server file to assign port dynamically.
+var port = process.env.PORT || 7070;
+
+//Making express to look in the public directory for (css, js, html .....).
+application.use(express.static(__dirname + '/public'));
+
+//Setting the home page
+application.get('/', function(request, response){
+	response.sendFile('public/crackjoycepurple.html' , { root : __dirname});
+	//response.sendFile('crackjoycepurple');
+});
+application.listen(port, function(){
+	console.log('Node Server is running on port: ' + port);
+});
